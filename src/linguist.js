eda82d1c02b73be47c6330e25d1bd2a911e89edb
Julien Kellerhals <julien.kellerhals@gmail.com>
2021-04-26 19:16:11 +0200

    Added output

--- src/linguist.js
+++ src/linguist.js
@@ -82,6 +82,9 @@ export const runLinguist = async (files) => {
   const stdout = await run("github-linguist --breakdown");
   const res = parseOutput(stdout);
 
+  console.log("Here")
+  console.log(res)
+
   const langs = Object.entries({ ...res })
     .reduce((acc, [name, v]) => {
       acc.push({
